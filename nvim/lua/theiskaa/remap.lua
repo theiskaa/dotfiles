42d55913ef72e2071cbdbeabab64ec0b77c46580
Ismael Sh <me@theiskaa.com>
2023-09-01 09:52:05 +0400

    feat(nvim): remove unused "rn"(rename) remap

--- nvim/lua/theiskaa/remap.lua
+++ nvim/lua/theiskaa/remap.lua
@@ -42,10 +42,9 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>rn", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
@@ -65,9 +64,9 @@ vim.keymap.set('n', 'fsh', ':split horizontal <CR>', { silent = true })
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
 -- Shorthand replace-all command
-vim.keymap.set('n', '<leader>rn', ':%s// /g')
 
 vim.keymap.set('n', '<leader>e', ':NvimTreeToggle <CR>')
