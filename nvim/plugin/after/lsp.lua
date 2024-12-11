c2d3c9159749eef46a0d91a8e1160026dabf3dcb
Ismael Sh <me@theiskaa.com>
2024-12-11 23:15:07 +0400

    feat(nvim): improve lsp configuration

--- nvim/plugin/after/lsp.lua
+++ nvim/plugin/after/lsp.lua
@@ -1,44 +1,44 @@
-local lsp = require("lsp-zero")
+local lsp = require('lsp-zero')
+lsp.preset('recommended')
 
-lsp.preset("recommended")
+-- Configure LSP logging
+vim.lsp.set_log_level('warn')
 
-lsp.ensure_installed({
-  'tsserver',
-  'rust_analyzer',
-  'pyright',
+-- Configure diagnostic settings
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = true,
+  float = {
+    focusable = false,
+    style = 'minimal',
+    border = 'rounded',
+    source = 'always',
+    header = '',
+    prefix = '',
+  },
 })
 
-lsp.configure('lua-language-server', {
-    settings = {}
-})
+-- Performance related settings
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.preselectSupport = true
+capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
+capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
+capabilities.textDocument.completion.completionItem.deprecatedSupport = true
+capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+  properties = {
+    'documentation',
+    'detail',
+    'additionalTextEdits',
+  }
+}
 
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
-})
-
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
-
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
-})
-
-lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
-})
-
-lsp.on_attach(function(client, bufnr)
+-- Configure buffer mappings when an LSP is attached
+local on_attach = function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
@@ -48,13 +48,62 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
   vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>rf", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end)
+end
 
-lsp.setup()
-
-vim.diagnostic.config({
-    virtual_text = true
+-- Setup Mason for LSP server management
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {
+    'lua_ls',
+    'gopls',
+    'pyright',
+    'rust_analyzer',
+    'ts_ls',
+  },
+  handlers = {
+    lsp.default_setup,
+  },
 })
+
+-- Configure specific LSP servers
+local lspconfig = require('lspconfig')
+
+-- Lua LSP settings
+lspconfig.lua_ls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = {'vim'},
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+})
+
+-- Go LSP settings
+lspconfig.gopls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+    },
+  },
+})
+
+-- Initialize LSP zero
+lsp.setup()
